84f38e7650a7da2120e9dbabcaebb00bf037a4f9
Neo Yin <yinzhang@berkeley.edu>
2023-10-04 13:02:36 -0700

    new scripts

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='heavily',
+      description='Trainings and deployments of heavy weight self-supervised learning (SSL) models from existing codebases.',
+      version='0.1',
+      packages=find_packages(),
+      install_requires=[])
