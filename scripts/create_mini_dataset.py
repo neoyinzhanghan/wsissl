10cd8081013a6ab522c8c0f08a37db6735beb557
Neo Yin <yinzhang@berkeley.edu>
2024-02-09 12:07:32 -0800

    creating mini dataset script

--- scripts/create_mini_dataset.py
+++ scripts/create_mini_dataset.py
@@ -1,6 +1,7 @@
 import os
 import random
 import pandas as pd
+import shutil
 from tqdm import tqdm
 
 # path to the current full dataset
@@ -16,7 +17,7 @@ num_test_per_class = 100
 # there should be three folders in the dataset_dir, train, val, and test
 # each of these folders should contain a subfolder named patch
 
-# randomly subssample the train, val, and test folders, and copy them to the mini_dataset_dir using the same folder structure and using symbolic link
+# randomly subssample the train, val, and test folders, and copy them to the mini_dataset_dir using the same folder structure
 for folder in ["train", "val", "test"]:
     os.makedirs(os.path.join(mini_dataset_dir, folder), exist_ok=True)
     os.makedirs(os.path.join(mini_dataset_dir, folder, "patch"), exist_ok=True)
@@ -33,4 +34,5 @@ for folder in ["train", "val", "test"]:
     selected_images = random.sample(images, num_per_class)
 
     for image in tqdm(selected_images, desc=folder):
-        os.symlink(os.path.join(dataset_dir, folder, "patch", image), os.path.join(mini_dataset_dir, folder, "patch", image))
+        # use shutil.copy to copy the images
+        shutil.copy(os.path.join(dataset_dir, folder, "patch", image), os.path.join(mini_dataset_dir, folder, "patch", image))
